31117321516c7c997275d543a976bf9bdaa5631d
CarlosHuaroc <63064560+CarlosHuaroc@users.noreply.github.com>
2024-06-05 03:25:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 34,
+        skills: ['JS', 'React', 'Node'],
+        isMarried: false
+    };
+
+    return member;
+}
